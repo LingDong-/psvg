774298d0e4992c10b88acceedf5efbbba02c7cfc
LingDong- <lingdong0618@hotmail.com>
2020-11-28 01:23:20 -0500

    new example

--- tools/compile_examples.js
+++ tools/compile_examples.js
@@ -3,11 +3,18 @@ const path = require('path');
 const { compilePSVG } = require('../dist/psvg');
 const examplePath = path.resolve(__dirname, '../examples');
 
+const overwrite = process.argv[2]||false;
+
 const examples = fs.readdirSync(examplePath).filter((x) => x.endsWith('.psvg'));
 
 for (const example of examples) {
   console.log(`compiling ${example}...`);
   const filepath = path.join(examplePath, example);
+  const outpath = filepath.replace(/\.psvg$/, '.svg');
+  if (fs.existsSync(outpath) && !overwrite) {
+    console.log(`skipped.`);
+    continue;
+  }
   const psvg = fs.readFileSync(filepath, 'utf-8');
   const svg = compilePSVG(psvg);
 
